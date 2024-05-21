42265cb4578cecd8e93a52064abec622b5e4d099
ChamodMullegama <chamod.mullegama@gmail.com>
2024-05-21 17:12:07 +0530

    reade ,write,update,rename and delete fiole useing asynchronous functions

--- index.js
+++ index.js
@@ -1,32 +1,61 @@
-const fs = require('fs');
+// const fs = require('fs');
+
+const fsPromises = require('fs').promises;
 const path = require('path');
 
-//reade file data
-fs.readFile(path.join(__dirname,'Files','lorem.txt') , 'utf8',(err , data) =>{
-   if (err) throw err;
-   console.log(data.toString())
-})
+const fileOps = async () => {
+    try {
 
-console.log('hello world')
+        //reade file
+        const data = await fsPromises.readFile(path.join(__dirname, 'Files', 'lorem.txt'), 'utf8');
+        console.log(data);
 
-// write file
-fs.writeFile(path.join(__dirname,'Files','reply.txt'),'Nice to meet you ',(err) =>{
-    if (err) throw err;
-    console.log('write complete')
-//update file
+        //delete file
+        await fsPromises.unlink(path.join(__dirname, 'Files', 'delete.txt'));
+        //write file
+        await fsPromises.writeFile(path.join(__dirname, 'Files', 'promisewrite.txt'), data);
+        //update file
+        await fsPromises.appendFile(path.join(__dirname, 'Files', 'promisewrite.txt'), '\n\nNice to meet you');
+        //rename file 
+        await fsPromises.rename(path.join(__dirname, 'Files', 'promisewrite.txt'), path.join(__dirname, 'Files', 'promisecomplete.txt'));
+        
+        //reade file
+        const newdata = await fsPromises.readFile(path.join(__dirname, 'Files', 'promisecomplete.txt'), 'utf8');
+        console.log(newdata);
+    } catch (error) {
+        console.error(error);
+    }
+};
 
-    fs.appendFile(path.join(__dirname,'Files','reply.txt'),'\n\nyes it is ',(err) =>{
-        if (err) throw err;
-        console.log('appent complete')
+fileOps();
 
-        //rename the file naem 
 
-        fs.rename(path.join(__dirname,'Files','reply.txt'),path.join(__dirname,'Files','newreply.txt'),(err) =>{
-            if (err) throw err;
-            console.log('rename complete')
-         })
-     })
- })
+// //reade file data
+// fs.readFile(path.join(__dirname,'Files','lorem.txt') , 'utf8',(err , data) =>{
+//    if (err) throw err;
+//    console.log(data.toString())
+// })
+
+// console.log('hello world')
+
+// // write file
+// fs.writeFile(path.join(__dirname,'Files','reply.txt'),'Nice to meet you ',(err) =>{
+//     if (err) throw err;
+//     console.log('write complete')
+// //update file
+
+//     fs.appendFile(path.join(__dirname,'Files','reply.txt'),'\n\nyes it is ',(err) =>{
+//         if (err) throw err;
+//         console.log('appent complete')
+
+//         //rename the file naem 
+
+//         fs.rename(path.join(__dirname,'Files','reply.txt'),path.join(__dirname,'Files','newreply.txt'),(err) =>{
+//             if (err) throw err;
+//             console.log('rename complete')
+//          })
+//      })
+//  })
 
 
 
