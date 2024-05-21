ecec5322c19bb2d815cd4825b6572e3c3adb4b13
ChamodMullegama <chamod.mullegama@gmail.com>
2024-05-21 16:06:23 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log("hello word")
+
+const  {add,sub,multi,dibide} = require('./math')
+
+console.log(add(1,2))
+console.log(sub(1,2))
+console.log(multi(1,2))
+console.log(dibide(1,2))
---
+++ math.js
@@ -0,0 +1,6 @@
+exports.add = (a,b) => a+b;
+exports.sub = (a,b) => a-b;
+exports.multi = (a,b) => a*b;
+exports.dibide = (a,b) => a/b;
+
+// module.exports = {add,sub,multi,dibide}
